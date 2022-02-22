09793b16abaddab830c37a92cbb25bd6ca7bd93e
BerdanKoc <Berdan.koc@epsi.fr>
2022-02-22 09:30:33 +0100

    tout

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>The Vanguardistes</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/customer.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="input-group">
+                    <div class="input-group-text" id="btnGroupAddon2">Recherche</div>
+                     <input type="text" class="form-control" placeholder="Recherche..." aria-label="Input group example" aria-describedby="btnGroupAddon2">
+                </div>
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a href="accueil.php" class="nav-link" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="telephone_mobile.php" class="nav-link" href="#">Téléphones Mobile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="a_propos.php" class="nav-link" href="#">A Propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contactez_nous.php" class="nav-link">Contactez Nous</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="faq.php" class="nav-link" href="#">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="expedition_et_retour.php" class="nav-link" href="#">Expédition Et Retour</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="politique.php" class="nav-link" href="#">Politique</a>
+                    </li>
+                </ul>
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                    <button type="button" class="btn btn-outline-primary">Connexion</button>
+                    <button type="button" class="btn btn-outline-primary">Inscription</button>
+                </div>
+            </div>
+            <div><img id="logo" src="Images/THe.png"></div>
+        </div>
+    </body>
+</html>
