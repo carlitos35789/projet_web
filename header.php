0f8c1a94ee7706ff41f243480f12361b44a0342f
BerdanKoc <Berdan.koc@epsi.fr>
2022-02-24 18:04:03 +0100

    maj_du_site

--- header.php
+++ header.php
@@ -6,44 +6,36 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>The Vanguardistes</title>
         <link href="css/bootstrap.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/customer.css">
+        <link rel="shortcut-icon" href="images/THe.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="css/custom.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous">
+        </script>
+
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous">
+        </script>
     </head>
+
     <body>
-        <div class="header">
-            <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
-                <div class="input-group">
-                    <div class="input-group-text" id="btnGroupAddon2">Recherche</div>
-                     <input type="text" class="form-control" placeholder="Recherche..." aria-label="Input group example" aria-describedby="btnGroupAddon2">
-                </div>
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a href="accueil.php" class="nav-link" aria-current="page" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="telephone_mobile.php" class="nav-link" href="#">Téléphones Mobile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="a_propos.php" class="nav-link" href="#">A Propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="contactez_nous.php" class="nav-link">Contactez Nous</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="faq.php" class="nav-link" href="#">FAQ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="expedition_et_retour.php" class="nav-link" href="#">Expédition Et Retour</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="politique.php" class="nav-link" href="#">Politique</a>
-                    </li>
-                </ul>
-                <div class="btn-group" role="group" aria-label="Basic outlined example">
-                    <button type="button" class="btn btn-outline-primary">Connexion</button>
-                    <button type="button" class="btn btn-outline-primary">Inscription</button>
-                </div>
+        <header>
+            <div class="navbar">
+                <nav class="menu">
+                    <img id="logo"src="images/THe.png" alt="" width="200px" height="100px">
+                    <a href="index.php" type="button" class="btn btn-primary btn-lg">Acceuil</a>
+                    <a href="apropos.php" type="button" class="btn btn-outline-primary">A Propos</a>
+                    <a href="contactez.php" type="button" class="btn btn-outline-primary">Contactez Nous</a>
+                    <a href="avis.php" type="button" class="btn btn-outline-primary">Avis</a>
+                    <a href="exp_et_re.php" type="button" class="btn btn-outline-primary">Expédition Et Retour</a>
+                </nav>
+                <nav class="connexion">
+                    <a href="connexion.php" type="button" class="btn btn-outline-primary">Connexion</a>
+                    <a href="inscription.php" type="button" class="btn btn-outline-primary">Inscription</a>
+                </nav>
             </div>
-            <div><img id="logo" src="Images/THe.png"></div>
-        </div>
-    </body>
-</html>
+        </header>
--- index.php
+++ index.php
@@ -1,21 +1,95 @@
+<?php require('header.php'); ?>
+    <section class="container-fluid banner">
+        <div class="ban">
+            <img src="images/image_tel2.jpg" alt="banniere"/>
+        </div>
+        <div class="inner-banner">
+            <h1>Soyez éco-responsable, pensez à nous !</h1>
+            <button type="button" class="btn btn-primary btn-lg" id="button-banner">Vendez maintenant !</button>
+        </div>
+    </section>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>The Vanguardistes</title>
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/customer.css">
-    </head>
-    <body>
-    <?php require('header.php'); ?>
-    <div class="text-start">
-        <h1 class="text-center">The Vanguardistes</h1>
+    <h1 class="text-carousel">Nous récupérons vos anciens mobiles ! </h1>
+
+    <div id="carouselExemple" class="carousel slide" data-ride="carousel" data-interval="5000">
+
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExemple" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExemple" data-slide-to="1"></li>
+            <li data-target="#carouselExemple" data-slide-to="2"></li>
+            <li data-target="#carouselExemple" data-slide-to="3"></li>
+            <li data-target="#carouselExemple" data-slide-to="4"></li>
+            <li data-target="#carouselExemple" data-slide-to="5"></li>
+        </ol>
+
+
+        <div class="carousel-inner">
+
+            <div class="carousel-item active">
+                <img src="images/iphone.jpg"
+                    class="d-block">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/samsung.jpg"
+                    class="d-block">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/huawei_tel.png"
+                class="d-block">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/lg.jpg"
+                    class="d-block">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/motorola.jpg"
+                class="d-block">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/oppo.jpg"
+                    class="d-block">
+            </div>
+        </div>
+
+        <a href="#carouselExemple" class="carousel-control-prev" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="ture"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a href="#carouselExemple" class="carousel-control-next" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
     </div>
-    <div <div class="text-center">
-        <img src="Images/image_casse.jpg" class="img-fluid" alt="...">
-    </div>
+    <script>
+        $('.carousel').carousel({
+
+            pause: "null"
+
+        })
+    </script>
+
+<h1 class="text2-carousel">En 3 étapes : </h1>
+
+<div class="step">
+    <a id="step1">
+        Etape 1: Remplissez le formulaire afin d'estimer votre mobile
+        <img src="images/formulaire.png" alt="" width="100px" height="100px">
+    </a>
+    <a id="step2">
+        Etape 2: Envoyez nous votre télephone
+        <img src="images/courrier.png" alt="" width="100px" height="100px">
+    </a>
+    <a id="step3">
+        Etape 3: Paiements dans un délai de 4 jours maximum
+        <img src="images/pay.png" alt="" width="100px" height="100px">
+    </a>
+</div>
+
 </body>
 </html>
