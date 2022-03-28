7f454ae89b44a088fd904c70130f485deef27ec8
BerdanKoc <Berdan.koc@epsi.fr>
2022-03-28 16:49:56 +0200

    update 16h50

--- exp_et_re.php
+++ exp_et_re.php
@@ -16,9 +16,4 @@ require('header.php');
                 </div>
             </div>    
         </section>
-        <div id="particles-js" class="background"></div>
-        <script src="js/particles.js"></script>
-        <script src="js/app.js"></script>
-        <script src="js/particles.min.js"></script>
-</body>
 </html>
