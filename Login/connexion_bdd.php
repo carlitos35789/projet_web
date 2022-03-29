bff01ab19a85e4c97a0a3418c5f9f07eec5d92cd
BerdanKoc <Berdan.koc@epsi.fr>
2022-03-29 14:42:36 +0200

    update 14h42

--- Login/connexion_bdd.php
+++ Login/connexion_bdd.php
@@ -1,6 +1,6 @@
 <?php
 try {
-        $mysqli = new PDO('mysql:host=localhost;dbname=telephone;charset=utf8;port=22', 'root', "");
+        $mysqli = new PDO('mariadb:host=localhost;dbname=telephone;charset=utf8;port=22', 'root', "");
     foreach($mysqli->query('SELECT * from telephone') as $row) {
         print_r($row);
     }
